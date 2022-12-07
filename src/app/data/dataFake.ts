286d350613e8e62b3c02fee818fb8a9b473429f7
Mirian Santos <mirian.trsantos@gmail.com>
2022-12-07 01:18:04 -0300

    components createds with styles

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Lançamento de Filmes e séries Marvel, em 2023",
+    "description": "A Marvel Studios está constantemente alterando as datas de lançamento de vários filmes inéditos em seu calendário. Um dos anúncios veio por meio de um emocionante trailer intitulado Comemoração aos Filmes da Marvel Studios, que trouxe um pouco de nostalgia ao mostrar cenas das fases anteriores e muita expectativa com imagens das próximas produções para os cinemas nos próximos anos.Confira como ficaram os próximos lançamentos do Universo Cinematográfico da Marvel",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
