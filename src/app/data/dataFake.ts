fdef0fa310d5564baabc46417bd4bba02d18c6b1
Mirian Santos <mirian.trsantos@gmail.com>
2022-12-07 10:03:00 -0300

    layout actualization

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,28 @@ export const dataFake = [
   {
     "id":"1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "description": "Marvel anuncia um novo filme do homem de ferro. As filmagens de Coração de Ferro já estão em andamento, as gravações se iniciaram em junho desse ano. A futura série do Disney+ estrelada por Dominique Thorne está prevista para estrear no outono de 2023, que acontece entre os meses de setembro a novembro.",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
     "id":"2",
     "title": "Lançamento de Filmes e séries Marvel, em 2023",
-    "description": "A Marvel Studios está constantemente alterando as datas de lançamento de vários filmes inéditos em seu calendário. Um dos anúncios veio por meio de um emocionante trailer intitulado Comemoração aos Filmes da Marvel Studios, que trouxe um pouco de nostalgia ao mostrar cenas das fases anteriores e muita expectativa com imagens das próximas produções para os cinemas nos próximos anos.Confira como ficaram os próximos lançamentos do Universo Cinematográfico da Marvel",
+    "description": "A Marvel Studios está constantemente alterando as datas de lançamento de vários filmes inéditos em seu calendário. Um dos anúncios veio por meio de um emocionante trailer intitulado Comemoração aos Filmes da Marvel Studios, que trouxe um pouco de nostalgia ao mostrar cenas das fases anteriores e muita expectativa com imagens das próximas produções para os cinemas nos próximos anos.Confira como ficaram os próximos lançamentos do Universo Cinematográfico da Marvel.",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+  },
+
+{
+  "id":"3",
+  "title": "Pantera Negra 2 estreia em 10 de novembro, com sessões de pré-estreia na noite anterior.",
+  "description": "Em preparação para a estreia de Wakanda Para Sempre dia 10/11 nos cinemas, o filme Pantera Negra (2018) já é o quarto conteúdo mais visto do streaming.",
+  "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+},
+
+{
+  "id":"4",
+  "title": "X-Men '97' estreia em 2023 e tem segunda temporada confirmada",
+  "description": "Na última sexta-feira (22), em um painel da San Diego Comic-Con 2022, a Marvel Studios agraciou os fãs com as novidades de suas próximas animações. Entre elas, X-Men '97'. O estúdio anunciou que a série chega ao catálogo da Disney+ entre os meses de setembro e novembro de 2023.",
+  "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339",
+}
+
 ]
